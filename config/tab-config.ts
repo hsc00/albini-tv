87801f3a46fb94cc7b731b8912dc26a2bc25b365
Henrique <hscoutinho00@gmail.com>
2025-04-05 16:41:52 +0100

    feat: tab file

--- config/tab-config.ts
+++
@@ -1,7 +0,0 @@
-export const tabConfig = [
-  { name: 'Streams1', url: 'https://daddylivehd1.click/wp-content/uploads/2024/10/daddylive.webp' },
-  { name: 'Streams2', url: 'https://www.pirilampo.tv/uploads/system_logo/logo.png' },
-  { name: 'Streams3', url: 'https://piratetv.pro/wp-content/themes/Pirate%20TV%20PRO%20theme/assets/images/piratetv-logo.png' },
-  { name: 'Football', url: 'https://img.icons8.com/?size=100&id=59&format=png&color=000000' },
-  { name: 'Motorsports', url: 'https://soccerlive.app/images/icons/f1-helmet.png?v2' },
-];
